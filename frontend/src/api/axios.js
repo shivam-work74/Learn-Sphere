be475fb25965e0a0f71f8060438401bbce8f21eb
shivam-work74 <shivam.work7488@gmail.com>
2025-09-17 17:45:50 +0530

    testing

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,11 +1,8 @@
 // src/api/axios.js
 import axios from 'axios';
 
-// This logic now sets the baseURL to the root of the server
 const API = axios.create({
-  baseURL: import.meta.env.PROD 
-    ? 'https://learnsphere-backend-g86a.onrender.com' // Production URL (no /api)
-    : 'http://localhost:5000',                       // Development URL (no /api)
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default API;

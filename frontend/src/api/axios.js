d1c66918265e04de3085976396ceac8dc24622b2
shivam-work74 <shivam.work7488@gmail.com>
2025-10-27 22:04:20 +0530

    updation

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,7 @@
 // src/api/axios.js
 import axios from 'axios';
 
-// This is the final, correct setup.
-// In production (Vercel), it uses the URL you provide.
-// In development (localhost), it uses the Vite proxy.
+
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL || '',
 });
